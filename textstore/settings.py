441e3012cffff1e625de37969ea5d5d688433a89
Lord Stark <strangexstark@gmail.com>
2025-08-22 21:54:42 +0530

    initial

--- textstore/settings.py
+++ textstore/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
